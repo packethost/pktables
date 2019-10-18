3e0d45343e80b39e0ba3cbce61ebf2ba65013935
Josh Goldman <joshgoldmanops@gmail.com>
2019-10-18 11:46:02 -0500

    Redid the variable grab to reflect the error message instead

--- pktables.py
+++ pktables.py
@@ -9,12 +9,12 @@ from netaddr import IPNetwork, IPAddress
 
 
 PACKETKEY = os.environ.get("PACKET_API_AUTH_TOKEN", os.environ.get("PACKETKEY"))
-PROJECTID = os.environ.get("PACKET_PROJECT", os.environ.get("PROJECTID"))
+PROJECTID = os.environ.get("PACKET_PROJECT_ID", os.environ.get("PROJECTID"))
 if PACKETKEY is None:
     raise RuntimeError("PACKET_API_AUTH_TOKEN or PACKETKEY(deprecated) env var not set")
 
 if PROJECTID is None:
-    raise RuntimeError("PACKET_PROJECT or PROJECTID(deprecated) env var not set")
+    raise RuntimeError("PACKET_PROJECT_ID or PROJECTID(deprecated) env var not set")
 
 RULESFILE = os.environ.get("RULESFILE", "/data/pktables.rules")
 CHAIN = os.environ.get("CHAIN", "PKTABLES")
